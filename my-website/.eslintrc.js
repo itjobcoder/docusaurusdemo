5e52996ab183cdf75b67bf86cd6b7b1ee0f1c2ae
Mohd <anwer.md@gmail.com>
2021-04-12 19:42:43 +0530

    Update .eslintrc.js

--- my-website/.eslintrc.js
+++ my-website/.eslintrc.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+/** const */
 const OFF = 0;
 const WARNING = 1;
 const ERROR = 2;
